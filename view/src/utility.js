3b1324b189c172d103070b06d0631c24bbf77a57
Himanshu Sharma <67851016honey59022@users.noreply.github.com>
2023-04-19 23:10:24 +0530

    changes

--- view/src/utility.js
+++ view/src/utility.js
@@ -1,2 +1,2 @@
 // export const BASE_URL="https://buildback.onrender.com";
-export const BASE_URL="https://buildback.onrender.com";
+export const BASE_URL="https://buildbackendfortreasurehunt.onrender.com";
