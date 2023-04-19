6ebb078a9fb0c835ee49d79a0d4974bb88b4a2d9
Himanshu Sharma <67851016honey59022@users.noreply.github.com>
2023-04-19 23:30:48 +0530

    changes

--- view/src/Components/Admin.js
+++ view/src/Components/Admin.js
@@ -55,12 +55,15 @@ export default function Admin({ userName, jwt, role }) {
         Admin Panel
         <button onClick={() => {window.localStorage.setItem("cred", "{}"); navigate("/");}}>Logout</button>
       </h1>
-      <div style={{ height: "87%", margin: 0, width: "100%", overflow: "auto", display: "flex", background: "#ffffff", flexDirection: "column", alignItems: "center", justifyContent: "space-evenly" }}>
-        {
-          userArr.map((obj, idx) => {
-            return <Box rank={idx} key={idx} totalPlayed={obj.totalPlayed} totalWon={obj.totalWon} percent={obj.percent} userName={obj.userName} />
-          })
-        }
+      <div style={{ height: "83%", margin: 0, width: "100%", overflow: "auto", display: "flex", flexDirection: "column", alignItems: "center"}}>
+        <div style={{height:"90%",width:"50%",overflow:"auto"}} className='example'>
+          {
+            userArr.map((obj, idx) => {
+              return <Box rank={idx} key={idx} totalPlayed={obj.totalPlayed} totalWon={obj.totalWon} percent={obj.percent} userName={obj.userName} />
+            })
+          }
+
+        </div>
       </div>
     </div>
   )
@@ -69,7 +72,7 @@ export default function Admin({ userName, jwt, role }) {
 
 function Box({ rank, totalPlayed, totalWon, percent, userName }) {
   return (
-    <div style={{ height: "5rem", width: "50%", background: "#ffffff", borderRadius: "6px", boxShadow: "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)", display: "flex", alignItems: "center", justifyContent: "center", justifyContent: "space-between" }}>
+    <div style={{ height: "100px", width: "99.7%",marginBottom:"1rem", background: "#ffffff", borderRadius: "6px", boxShadow: "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)", display: "flex", alignItems: "center", justifyContent: "center", justifyContent: "space-between" }}>
       <h3 style={{ height: "100%", width: "10%", background: "", display: "flex", alignItems: "center", justifyContent: "center", fontFamily: "monospace", margin: 0 }}>Rank {rank + 1}</h3>
       <h3 style={{ height: "100%", width: "10%", background: "", display: "flex", alignItems: "center", justifyContent: "center", fontFamily: "monospace", margin: 0 }}>{userName}</h3>
       <div style={{ height: "100%", width: "25%", background: "", display: "flex", alignItems: "center", justifyContent: "center", fontFamily: "monospace", fontSize: "0.9rem" }}>Total Match Played {totalPlayed}</div>
